795cca07e5c049952958bf4dcb0001462c973b4a
Sergei <sergflutterdev@gmail.com>
2026-04-29 23:28:56 +0500

    Release 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+## 1.0.2
+
 * Improved SDK log visibility
 
 ## 1.0.1
--- Sources/GravitySDK/utils/GravitySDKInfo.swift
+++ Sources/GravitySDK/utils/GravitySDKInfo.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 final class GravitySDKInfo {
-    static let version: String = "1.0.1"
+    static let version: String = "1.0.2"
 }
